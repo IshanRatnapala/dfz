12d13ec0b31dfc21a746ad370287e80932b9b7e9
Dragfreeze <Dragfreeze@gmail.com>
2017-01-28 13:36:22 +0530

    add env port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var middleware = require('./middleware');
 var app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(middleware.logger);
 
